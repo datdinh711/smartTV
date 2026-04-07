7f675b7c21b6fe742178c386ff3851c0b7419386
datdinh711 <datdinh711@gmail.com>
2026-04-07 21:51:11 +0700

    ✨feat(#36): add home button for business page (#37)

--- src/app/features/strategy/strategy.component.ts
+++ src/app/features/strategy/strategy.component.ts
@@ -1,13 +1,12 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { HomeButtonComponent } from '@shared/components';
 
 @Component({
   selector: 'app-strategy',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HomeButtonComponent],
   templateUrl: './strategy.component.html',
   styleUrl: './strategy.component.scss',
 })
-
 export class StrategyComponent {}
