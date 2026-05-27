93125634cd394758dd6f7149359c04884579daca
datdinh711 <datdinh711@gmail.com>
2026-05-27 23:23:59 +0700

    70: implement animal health management page (#72)

--- src/app/features/strategy/components/sustainability-vdo/sustainability-vdo.component.ts
+++ src/app/features/strategy/components/sustainability-vdo/sustainability-vdo.component.ts
@@ -1,22 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { APP_ROUTES } from '@core/constants';
-import { NavigationHistoryService, NavigatorService } from '@core/services';
+import { NavigatorService } from '@core/services';
 import { STRATEGY_ROUTES } from '@features/strategy/constants/strategy-route.constant';
 import { TranslateService } from '@ngx-translate/core';
 import {
-  HomeButtonComponent,
   NavigationButtonComponent,
-  VideoPlayerComponent,
+  VideoPlayerComponent
 } from '@shared/components';
 
 @Component({
   selector: 'app-sustainability-vdo',
   standalone: true,
-  imports: [NavigationButtonComponent, HomeButtonComponent, VideoPlayerComponent],
+  imports: [NavigationButtonComponent, VideoPlayerComponent],
   templateUrl: './sustainability-vdo.component.html',
   styleUrl: './sustainability-vdo.component.scss',
 })
-export class SustainabilityVdoComponent implements OnInit {
+export class SustainabilityVdoComponent {
   farmingPillarsPath = `${APP_ROUTES.STRATEGY}/${STRATEGY_ROUTES.SUSTAINABILITY_REGCONIGTION}`;
 
   APP_ROUTE = APP_ROUTES;
@@ -27,18 +26,14 @@ export class SustainabilityVdoComponent implements OnInit {
     `${APP_ROUTES.STRATEGY}/${STRATEGY_ROUTES.SUSTAINABILITY_REGCONIGTION}`;
 
   constructor(
-    private readonly _navigationHistoryService: NavigationHistoryService,
     private readonly _navigatorService: NavigatorService,
     private readonly _translate: TranslateService,
-  ) {}
+  ) { }
 
   get videoSrc(): string {
     return this._translate.getCurrentLang() === 'en'
       ? 'assets/videos/Sustainability_VDO_2026_EN_sub.mp4'
       : 'assets/videos/Sustainability_VDO_2026_VN_sub.mp4';
-  }
-
-  ngOnInit(): void {
   }
 
   onVideoEnded(): void {
